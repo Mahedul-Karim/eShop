3da87eb06d672ae06e5a93572a659cc0ff9dd3c4
Mahedul-Karim <karimrupo21@gmail.com>
2023-10-28 17:05:00 +0600

    slider

--- frontend/src/components/layout/Hero.jsx
+++ frontend/src/components/layout/Hero.jsx
@@ -27,51 +27,48 @@ const SLIDES = [
     id: 4,
     src: "/assets/slide-2.jpg",
     alt: "slide img 2",
-  }
+  },
 ];
 const Hero = () => {
   const [curSlide, setCurSlide] = useState(0);
   const containerRef = useRef();
-  const [scr,setScr]=useState()
   
-  useEffect(()=>{
-    setScr(containerRef?.current?.firstElementChild?.offsetWidth)
-  },[containerRef])
 
   const nextSlide = () => {
-    // if (curSlide === SLIDES.length - 1) {
-    //   return;
-    // }
-    setCurSlide((prev) => (prev + 1));
-    
-    setScr(prev => prev+prev)
-    // containerRef.current.scrollLeft = scr ;
-    // console.log(containerRef.current.scrollLeft)
-    containerRef.current.style.transform=`translateX(-${containerRef?.current?.firstElementChild?.offsetWidth}px)`
+    // setCurSlide((prev) => (prev === SLIDES.length - 1 ? 0 : prev + 1));
+    if(curSlide === SLIDES.length - 2){
+      SLIDES.push(SLIDES[0])
+    }
+    setCurSlide(prev => prev+1)
   };
 
   const prevSlide = () => {
-    // if (curSlide === 0) {
-    //   return;
-    // }
-    // setCurSlide((prev) => (prev - 1));
-    containerRef.current.style.transform=`translateX(${containerRef?.current?.firstElementChild?.offsetWidth}px)`;
+    setCurSlide((prev) => (prev === 0 ? SLIDES.length - 1 : prev - 1));
   };
 
   return (
     <Container
       styles={"grid grid-cols-1 800px:grid-cols-[1fr_0.4fr] mt-6 gap-4"}
     >
-      <div className="overflow-hidden relative">
+      <div className="overflow-hidden relative w-full h-full">
         <div
-          className="grid grid-flow-col auto-cols-[100%] transition-all"
+          className="flex transition-all w-full h-full"
           style={{
             transitionDuration: "0.6s",
           }}
           ref={containerRef}
         >
           {SLIDES.map((slider, i) => (
-            <div id="slide" data-img={i} key={slider.id}>
+            <div
+              id="slide"
+              data-img={i}
+              key={slider.id}
+              className="flex-shrink-0 w-full"
+              style={{
+                transform: `translateX(-${curSlide * 100}%)`,
+                transition: "all 0.6s",
+              }}
+            >
               <img
                 src={slider.src}
                 className="w-full object-cover h-auto"
