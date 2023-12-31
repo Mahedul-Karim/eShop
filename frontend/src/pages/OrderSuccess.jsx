1be68c0ff8989c092a28170ea34442250a78e94b
Mahedul-Karim <karimrupo21@gmail.com>
2023-12-31 16:24:44 +0600

    order bug fix

--- frontend/src/pages/OrderSuccess.jsx
+++ frontend/src/pages/OrderSuccess.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
-import Lottie from "react-lottie";
-import animationData from '/assets/107043-success.json?url';
+
 
 const Success = () => {
-    const defaultOptions = {
-      loop: false,
-      autoplay: true,
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice",
-      },
-    };
+    
     return (
       <div>
-        <Lottie options={defaultOptions} width={300} height={300} />
+        {/* <Lottie options={defaultOptions} width={300} height={300} /> */}
         <h5 className="text-center mb-14 text-[25px] text-[#000000a1]">
           Your order is successful 😍
         </h5>
